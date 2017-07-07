1eb72165dcffe21381610a0ec2f08b6af32e2394
Carlos Filoteo <c.filoteo@live.com>
2017-07-06 23:34:27 -0600

    Begin converting index.html into App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'font-awesome/css/font-awesome.css';
 import './App.css';
+import Page from './components/Page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	    <Page>
+		    <header id="header" className="alt">
+			    <h1>Solid State</h1>
+			    <nav>
+				    <a href="#menu">Menu</a>
+			    </nav>
+		    </header>
+	    </Page>
     );
   }
 }
