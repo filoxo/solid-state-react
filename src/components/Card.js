501ade25ff1900acc64addf4068d44952b03e422
Carlos Filoteo <c.filoteo@live.com>
2018-10-21 22:35:18 -0600

    Refactor Card based on findings from test

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,11 @@ import * as React from 'react'
 import './Card.css'
 
 type Props = {
-  imgSrc: string,
-  imgAlt: string,
-  title: string,
-  desc: string,
-  to: string,
+  imgSrc: ?string,
+  imgAlt: ?string,
+  title: ?string,
+  desc: ?string,
+  to: ?string,
 }
 type State = {}
 
@@ -15,26 +15,28 @@ export default class Card extends React.Component<Props, State> {
   render() {
     const { imgSrc, imgAlt, title, desc, to } = this.props
     let img = null
-    if (imgSrc !== undefined) {
-      img = <img src={imgSrc} alt={imgAlt} />
-      if (to !== undefined) {
-        img = (
-          <a href={to} className="image">
-            {img}
-          </a>
+    if (imgSrc) {
+      if (!imgAlt)
+        console.warn(
+          '<Card/>: Provide an imgAlt prop in addition to imgSrc for improved accessibility.'
         )
-      } else {
-        img = <div className="image">{img}</div>
-      }
+      const imgEl = <img src={imgSrc} alt={imgAlt} />
+      img = to ? (
+        <a href={to} className="image">
+          {imgEl}
+        </a>
+      ) : (
+        <div className="image">{imgEl}</div>
+      )
     }
 
     return (
       <article className="Card">
         {img}
         <div className="CardContent">
-          {title !== undefined && <h3 className="major">{title}</h3>}
-          {desc !== undefined && <p>{desc}</p>}
-          {to !== undefined && (
+          {title && <h3 className="major">{title}</h3>}
+          {desc && <p>{desc}</p>}
+          {to && (
             <a href={to} className="special">
               Learn more
             </a>
