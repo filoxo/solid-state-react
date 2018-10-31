14a6f57cf1d25c3501903122b2adc3f8fb203c4f
Carlos <filoxo@users.noreply.github.com>
2018-10-30 22:12:17 -0600

    CSS modules for Menu (#20)
    * Refactor Menu to fix bodyOpenClassName, remove FocusTrap, convert to functional component
    
    * Add contentLabel to Menu
    
    * Convert menu styles; add MenuLinks component; fix body class prop
    
    * Format
    
    * Update usage of Menu, add MenuLinks

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,48 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Modal from 'react-modal'
 
-import FocusTrap from 'react-focus-trap'
-import './Menu.css'
+import styles from './Menu.module.css'
 
 Modal.setAppElement('#root')
 
-class Menu extends Component {
-  componentDidUpdate() {
-    if (this.props.showMenu) {
-      document.body.classList.add('blur')
-    } else {
-      document.body.classList.remove('blur')
-    }
-  }
-  render() {
-    const { showMenu, closePortal, children } = this.props
-    return (
-      <Modal
-        isOpen={showMenu}
-        onRequestClose={this.closePortal}
-        className="Modal"
-        overlayClassName="backdrop"
-      >
-        <nav>
-          <FocusTrap>
-            <div className="menu">
-              {children}
-              <button className="close" type="button" onClick={closePortal}>
-                Close
-              </button>
-            </div>
-          </FocusTrap>
-        </nav>
-      </Modal>
-    )
-  }
-}
+const Menu = ({ showMenu, closePortal, children }) => (
+  <Modal
+    isOpen={showMenu}
+    onRequestClose={closePortal}
+    className={styles.modal}
+    overlayClassName={styles.backdrop}
+    bodyOpenClassName={styles.blur}
+    contentLabel="Open site menu"
+  >
+    <nav className={styles.menu}>
+      {children}
+      <button className={styles.close} type="button" onClick={closePortal}>
+        Close
+      </button>
+    </nav>
+  </Modal>
+)
+
+const MenuLinks = ({ children }) => (
+  <ul className={styles.links}>
+    {children.map(child => (
+      <li className={styles.link}>{child}</li>
+    ))}
+  </ul>
+)
 
 const MenuTrigger = ({ onClick }) => (
-  <button className="menuBtn" type="button" onClick={onClick}>
+  <button className={styles.trigger} type="button" onClick={onClick}>
     Menu
   </button>
 )
 
 export default Menu
-export { Menu, MenuTrigger }
+export { Menu, MenuLinks, MenuTrigger }
